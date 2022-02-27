708e484ea932a07584cee276b660050688177379
x-in <xinliang.zhu@qq.com>
2022-02-28 00:02:37 +0800

    feat: 实验：vue2生命周期钩子beforeCreate中配置Vuex的actions调用接口。

---
+++ src/vue-lifecycle/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" class="btn btn"  @click="click">点击改变</button>
+    <h2>{{userInfo}}</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  beforeCreate() {
+    console.log(this);
+     this.$store.dispatch('getUserInfo')
+  },
+  methods: {
+    click() {
+      this.$store.dispatch('change','hi 你好')
+    }
+  },
+  computed: mapState({
+    userInfo: state => state
+  }),
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
