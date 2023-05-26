c41410722971f8d20081a53ef78deee5a4e6d682
ZXinL-CN <xinliang.zhu@qq.com>
2023-05-26 11:17:16 +0800

    feature(): update.

---
+++ src/L78Z/0526.js
@@ -0,0 +1,300 @@
+const jsonData = {
+    "record": {
+        "version_no": "26",
+        "metadata_scheme_name": "规划审批档案（卷内）",
+        "block": [
+            {
+                "name": "归档信息",
+                "block": [
+                    {
+                        "name": "资源标识",
+                        "property": [
+                            {
+                                "name": "archive_1st_category",
+                                "title": "一级门类编码"
+                            },
+                            {
+                                "name": "fonds_id",
+                                "title": "全宗号"
+                            },
+                            {
+                                "name": "XH",
+                                "title": "序号",
+                                "content": "1"
+                            },
+                            {
+                                "name": "da_label_name",
+                                "title": "标签名称"
+                            },
+                            {
+                                "name": "archival_id",
+                                "title": "档号",
+                                "content": "113322"
+                            },
+                            {
+                                "name": "archives_code",
+                                "title": "档案馆代码"
+                            },
+                            {
+                                "name": "category_code",
+                                "title": "门类编码"
+                            }
+                        ],
+                        "archive_1st_category": "DA-CSML",
+                        "category_code": "DA-CSML"
+                    },
+                    {
+                        "name": "访问控制信息",
+                        "property": [
+                            {
+                                "name": "security_class",
+                                "title": "密级"
+                            },
+                            {
+                                "name": "open_class",
+                                "title": "开放状态"
+                            },
+                            {
+                                "name": "released_network",
+                                "title": "发布网段"
+                            },
+                            {
+                                "name": "decryption_status",
+                                "title": "解密标识"
+                            },
+                            {
+                                "name": "secrecy_period",
+                                "title": "保密期限"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "说明信息",
+                        "property": [
+                            {
+                                "name": "retention_period",
+                                "title": "保管期限",
+                                "content": "3"
+                            },
+                            {
+                                "name": "fonds_name",
+                                "title": "全宗名称"
+                            },
+                            {
+                                "name": "physical_archive_location",
+                                "title": "实体存址号"
+                            },
+                            {
+                                "name": "file_year",
+                                "title": "年度",
+                                "content": "2023"
+                            },
+                            {
+                                "name": "page_num",
+                                "title": "张页号"
+                            },
+                            {
+                                "name": "archives_num",
+                                "title": "归档份数",
+                                "content": "1"
+                            },
+                            {
+                                "name": "filed_date",
+                                "title": "归档时间"
+                            },
+                            {
+                                "name": "filed_by",
+                                "title": "归档者"
+                            },
+                            {
+                                "name": "doc_date",
+                                "title": "形成日期",
+                                "content": "20230518"
+                            },
+                            {
+                                "name": "digitized_status",
+                                "title": "数字化状态"
+                            },
+                            {
+                                "name": "owner",
+                                "title": "文件所有者"
+                            },
+                            {
+                                "name": "exist_document",
+                                "title": "是否有原文",
+                                "content": "false"
+                            },
+                            {
+                                "name": "fonds_unit",
+                                "title": "立档单位"
+                            },
+                            {
+                                "name": "author",
+                                "title": "责任者",
+                                "content": "马牛逼"
+                            },
+                            {
+                                "name": "material_num",
+                                "title": "载体数量"
+                            },
+                            {
+                                "name": "carrier_type",
+                                "title": "载体类型",
+                                "content": "02"
+                            },
+                            {
+                                "name": "title",
+                                "title": "题名",
+                                "content": "装逼集锦"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "管理信息",
+                        "property": [
+                            {
+                                "name": "out_houseware_status",
+                                "title": "出库状态"
+                            },
+                            {
+                                "name": "rectification_desc",
+                                "title": "整改情况说明"
+                            },
+                            {
+                                "name": "rectification_deadline",
+                                "title": "整改截止时间"
+                            },
+                            {
+                                "name": "transfer_batch",
+                                "title": "移交批次"
+                            },
+                            {
+                                "name": "transfer_mode",
+                                "title": "移交方式"
+                            },
+                            {
+                                "name": "transfer_date",
+                                "title": "移交时间"
+                            },
+                            {
+                                "name": "transfer_by",
+                                "title": "移交者"
+                            },
+                            {
+                                "name": "identity_by",
+                                "title": "鉴定人"
+                            },
+                            {
+                                "name": "identity_date",
+                                "title": "鉴定时间"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "name": "业务内容",
+                "block": [
+                    {
+                        "name": "过程信息",
+                        "property": [
+                            {
+                                "name": "body",
+                                "title": "意见内容"
+                            },
+                            {
+                                "name": "type",
+                                "title": "意见类型"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "内容描述",
+                        "property": [
+                            {
+                                "name": "SYJD",
+                                "title": "上一阶段",
+                                "content": "6"
+                            },
+                            {
+                                "name": "JS",
+                                "title": "件数",
+                                "content": "11"
+                            },
+                            {
+                                "name": "remark",
+                                "title": "备注",
+                                "content": "未成年人勿看"
+                            },
+                            {
+                                "name": "KFDBS",
+                                "title": "开放度标识",
+                                "content": "02"
+                            },
+                            {
+                                "name": "SSJD",
+                                "title": "所属阶段",
+                                "content": "3"
+                            },
+                            {
+                                "name": "WJBH",
+                                "title": "文件编号",
+                                "content": "321123"
+                            },
+                            {
+                                "name": "GHSLH",
+                                "title": "规划受理号",
+                                "content": "1"
+                            },
+                            {
+                                "name": "YH",
+                                "title": "页号",
+                                "content": "1"
+                            },
+                            {
+                                "name": "YS",
+                                "title": "页数",
+                                "content": "12"
+                            },
+                            {
+                                "name": "RQ",
+                                "title": "日期",
+                                "content": null
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "name": "电子文件"
+            }
+        ],
+        "metadata_scheme_code": "GHSP-ITEM",
+        "create_date": "2023-05-24",
+        "modify_date": "2023-05-24",
+        "template_id": "e0ea976d-9dd6-42ab-8ee9-70034ba521a9",
+        "name": "规划审批档案（卷内）",
+        "id": "9792a069-89ef-45d7-a2b6-e1a92584084d"
+    }
+}
+
+function filterEmptyContent(jsonData) {
+    function filterContent(obj) {
+      if (Array.isArray(obj)) {
+        obj.forEach(filterContent);
+      } else if (typeof obj === 'object' && obj !== null) {
+        for (const key in obj) {
+          if (key === 'content' && (obj[key] === null || obj[key] === '')) {
+            delete obj[key];
+          } else {
+            filterContent(obj[key]);
+          }
+        }
+      }
+    }
+  
+    filterContent(jsonData);
+    return jsonData;
+  }
+  
+  const filteredJsonData = filterEmptyContent(jsonData);
